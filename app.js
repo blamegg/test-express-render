cfea6be69ea3d684b4a1a230975d0653de3e8442
shivang <shivang.ramola.hp@gmail.com>
2024-05-30 11:02:18 +0530

    setup files

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/randomtest", (req, res) => {
+  res.status(200).send({
+    id: 1,
+    context: "testing api express",
+  });
+});
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -58,4 +67,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
